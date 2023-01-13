8dd619697c2f3acb62f7747dc86838e24d23af8e
Ed <ed@xmos.com>
2023-01-13 12:42:01 +0000

    Enable BCLK monitoring and add random jitter to pt sampling

--- examples/i2s_slave/src/i2s_slave_sw_pll.c
+++ examples/i2s_slave/src/i2s_slave_sw_pll.c
@@ -12,7 +12,7 @@
 
 #define MCLK_FREQUENCY              12288000
 #define I2S_FREQUENCY               48000
-#define PLL_RATIO                   (MCLK_FREQUENCY / REF_FREQUENCY)
+#define PLL_RATIO                   (MCLK_FREQUENCY / I2S_FREQUENCY)
 #define CONTROL_LOOP_COUNT          512
 #define PPM_RANGE                   150
 
@@ -45,16 +45,25 @@ typedef struct i2s_callback_args_t {
     bool did_restart;                       // Set by init
     int lock_status;
     int32_t loopback_samples[NUM_I2S_CHANNELS];
-    port_t p_mclk_count;                    // Used for keeping track of MCLK output for clock recovery builds
+    port_t p_mclk_count;                    // Used for keeping track of MCLK output for sw_pll
+    port_t p_bclk_count;                    // Used for keeping track of BCLK input for sw_pll
+    xclock_t i2s_ck_bclk;
     sw_pll_state_t *sw_pll;                 // Pointer to sw_pll state (if used)
 
 } i2s_callback_args_t;
 
 
+uint32_t random = 0x80085; //Initial seed
+void pseudo_rand_uint32(uint32_t *r){
+    #define CRC_POLY (0xEB31D82E)
+    asm volatile("crc32 %0, %2, %3" : "=r" (*r) : "0" (*r), "r" (-1), "r" (CRC_POLY));
+}
+
 I2S_CALLBACK_ATTR
 static void i2s_init(void *app_data, i2s_config_t *i2s_config){
     printf("I2S init\n");
     i2s_callback_args_t *cb_args = app_data;
+
     i2s_config->mode = I2S_MODE_I2S;
     i2s_config->mclk_bclk_ratio = (MCLK_FREQUENCY / I2S_FREQUENCY);
 
@@ -63,7 +72,25 @@ static void i2s_init(void *app_data, i2s_config_t *i2s_config){
 
 I2S_CALLBACK_ATTR
 static i2s_restart_t i2s_restart_check(void *app_data){
-    printf("loop\n");
+    i2s_callback_args_t *cb_args = app_data;
+
+    // Add random jitter
+    hwtimer_t tmr = hwtimer_alloc();
+    pseudo_rand_uint32(&random);
+    hwtimer_delay(tmr, random & 0x3f);
+    hwtimer_free(tmr);
+
+    static uint16_t old_mclk_pt = 0;
+    static uint16_t old_bclk_pt = 0;
+
+    uint16_t mclk_pt = port_get_trigger_time(cb_args->p_mclk_count);
+    uint16_t bclk_pt = port_get_trigger_time(cb_args->p_bclk_count);
+    
+    printf("%u %u\n", mclk_pt - old_mclk_pt, bclk_pt - old_bclk_pt);
+
+    old_mclk_pt = mclk_pt;
+    old_bclk_pt = bclk_pt;
+
     return I2S_NO_RESTART;
 }
 
@@ -90,23 +117,38 @@ static void i2s_receive(void *app_data, size_t num_in, const int32_t *i2s_sample
 
 void sw_pll_test(void){
 
-    xclock_t clk_mclk = XS1_CLKBLK_1;
-    port_t p_ref_clk = PORT_I2S_LRCLK;
-    xclock_t clk_word_clk = XS1_CLKBLK_2;
-    port_t p_ref_clk_count = XS1_PORT_32A;
-
-
+    // I2S resources
     port_t p_i2s_dout[NUM_I2S_LINES] = {PORT_I2S_DATA1};
     port_t p_i2s_din[NUM_I2S_LINES] = {PORT_I2S_DATA0};
     port_t p_bclk = PORT_I2S_BCLK;
     port_t p_lrclk = PORT_I2S_LRCLK;
-    port_t p_mclk = PORT_MCLK;
-    port_t p_mclk_count = PORT_MCLK_COUNT;
-    xclock_t ck_bclk = XS1_CLKBLK_3;
+    xclock_t i2s_ck_bclk = XS1_CLKBLK_1;
 
-    port_enable(p_mclk);
     port_enable(p_bclk);
     // NOTE:  p_lrclk does not need to be enabled by the caller
+
+
+    // sw-pll resources
+    port_t p_mclk = PORT_MCLK;
+    port_t p_mclk_count = PORT_MCLK_COUNT;
+    xclock_t clk_mclk = XS1_CLKBLK_2;
+    port_t p_bclk_count = XS1_PORT_16A;
+
+    // Create clock from mclk port and use it to clock clk_mclk.
+    port_enable(p_mclk);
+    clock_enable(clk_mclk);
+    clock_set_source_port(clk_mclk, p_mclk);
+
+    // Clock p_mclk_count from clk_mclk
+    port_enable(p_mclk_count);
+    port_set_clock(p_mclk_count, clk_mclk);
+
+    clock_start(clk_mclk);
+
+
+    // Create use bclk clckblock to clock p_bclk_count
+    port_enable(p_bclk_count);
+    port_set_clock(p_bclk_count, i2s_ck_bclk);
 
     // setup_ref_and_mclk_ports_and_clocks(p_mclk, clk_mclk, p_ref_clk, clk_word_clk, p_ref_clk_count);
 
@@ -115,19 +157,19 @@ void sw_pll_test(void){
     // port_t p_recovered_ref_clk = PORT_I2S_DAC_DATA;
     // setup_recovered_ref_clock_output(p_recovered_ref_clk, clk_recovered_ref_clk, p_mclk, PLL_RATIO);
     
-    // sw_pll_state_t sw_pll;
-    // sw_pll_init(&sw_pll,
-    //             SW_PLL_15Q16(0.0),
-    //             SW_PLL_15Q16(1.0),
-    //             SW_PLL_15Q16(0.01),
-    //             CONTROL_LOOP_COUNT,
-    //             PLL_RATIO,
-    //             frac_values_80,
-    //             SW_PLL_NUM_LUT_ENTRIES(frac_values_80),
-    //             APP_PLL_CTL_12288,
-    //             APP_PLL_DIV_12288,
-    //             APP_PLL_NOMINAL_INDEX_12288,
-    //             PPM_RANGE);
+    sw_pll_state_t sw_pll;
+    sw_pll_init(&sw_pll,
+                SW_PLL_15Q16(0.0),
+                SW_PLL_15Q16(1.0),
+                SW_PLL_15Q16(0.01),
+                CONTROL_LOOP_COUNT,
+                PLL_RATIO,
+                frac_values_80,
+                SW_PLL_NUM_LUT_ENTRIES(frac_values_80),
+                APP_PLL_CTL_12288,
+                APP_PLL_DIV_12288,
+                APP_PLL_NOMINAL_INDEX_12288,
+                PPM_RANGE);
 
 
     // Initialise app_data
@@ -136,7 +178,9 @@ void sw_pll_test(void){
         .lock_status = SW_PLL_LOCKED,
         .loopback_samples = {0},
         .p_mclk_count = p_mclk_count,
-        // .sw_pll = &sw_pll
+        .p_bclk_count = p_bclk_count,
+        .i2s_ck_bclk = i2s_ck_bclk,
+        .sw_pll = &sw_pll
     };
 
     // Initialise callback function pointers
@@ -157,7 +201,7 @@ void sw_pll_test(void){
             NUM_I2S_LINES,
             p_bclk,
             p_lrclk,
-            ck_bclk);
+            i2s_ck_bclk);
 
 
         // port_in(p_ref_clk_count);   // This blocks each time round the loop until it can sample input (rising edges of word clock). So we know the count will be +1 each time.
