c50002297038ddf1db2f7671bcf9e4bf48459d50
Huw Percival <huwpercival@xmos.com>
2023-01-23 10:29:23 +0000

    test works when I overwrite the ppm value

--- tests/test_lib_sw_pll.py
+++ tests/test_lib_sw_pll.py
@@ -98,6 +98,7 @@ def test_example(request):
 
     # Generate init parameters
     sol = pll_solution(xtal_freq, target_mclk_f)
+    sol.ppm = 1000
     start_reg = sol.lut.get_lut()[0]
     args = DutArgs(
             kp=q16(0),
@@ -115,13 +116,12 @@ def test_example(request):
             lut=sol.lut.get_lut(),
     )
 
+    pll = app_pll_frac_calc(
+        xtal_freq, sol.F, sol.R, sol.OD, sol.ACD, 1, 2
+    )
+    pll.update_pll_frac_reg(start_reg)
 
     with Dut(args) as dut:
-        pll = app_pll_frac_calc(
-            xtal_freq, sol.F, sol.R, sol.OD, sol.ACD, 1, 2
-        )
-        pll.update_pll_frac_reg(start_reg)
-        print(pll.get_output_freqency())
         assert (-1, int(sol.lut.get_lut()[0])) == dut.do_control(0, 0)
 
         ref_pt = 0
@@ -132,10 +132,12 @@ def test_example(request):
             # while locked != 0:
             time = 0
             for _ in range(100):
-                print("elapsed r, m: ", ref_pt_per_loop, loop_time*pll.get_output_freqency())
-
-                mclk_count = loop_time * pll.get_output_freqency()
+                # this basic test has fixed ref clock, the sunny day case
                 ref_pt = (ref_pt + ref_pt_per_loop)
+
+                # increment the mclk count based on the frequency that was
+                # set by the 
+                mclk_count = loop_time * pll.get_output_freqency()
                 mclk_pt = (mclk_pt + mclk_count)
                 locked, reg = dut.do_control(int(mclk_pt % 2**16), int(ref_pt % 2**16))
                 
@@ -156,6 +158,7 @@ def test_example(request):
             df = pandas.DataFrame(results)
             df.plot("time", ["target", "mclk"]).get_figure().savefig(f"{request.node.name}-freqs.png")
             df.plot("time", ["exp_mclk_count", "mclk_count"]).get_figure().savefig(f"{request.node.name}-counts.png")
+            df.to_csv(f"{request.node.name}.csv")
 
 
         
