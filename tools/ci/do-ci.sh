97e10a4b49a6e2ea6b0ccd3d857c2a4a7a5bb78f
Ed <ed@xmos.com>
2023-02-21 08:58:36 +0000

    Build examples in CI too

--- tools/ci/do-ci.sh
+++ tools/ci/do-ci.sh
@@ -7,6 +7,10 @@ set -ex
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=modules/xcore_sdk/xmos_cmake_toolchain/xs3a.cmake -DXCORE_SDK_DIR=modules/xcore_sdk
 cmake --build build --target all --target test_app -j$(nproc)
 
+# Build examples
+cmake --build build --target all --target simple -j$(nproc)
+cmake --build build --target all --target i2s_slave -j$(nproc)
+
 pushd tests
 pytest --junitxml=results.xml -rA -v --durations=0 -o junit_logging=all
 ls bin
