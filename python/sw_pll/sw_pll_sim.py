35be636663f27b9d15901751eb58c00e1ed8b7ea
Ed <ed@xmos.com>
2023-03-21 09:44:19 +0000

    doc name and profile extend to 1000ppm item

--- python/sw_pll/sw_pll_sim.py
+++ python/sw_pll/sw_pll_sim.py
@@ -231,13 +231,13 @@ def parse_register_file(register_file):
 
 
 
-                                                              # see /doc/generating_lut_guide.rst for guidance on these settings
+                                                              # see /doc/sw_pll.rst for guidance on these settings
 def get_pll_solution(input_frequency, target_output_frequency, max_denom=80, min_F=200, ppm_max=2, fracmin=0.65, fracmax=0.95):
     """
         This is a wrapper function for pll_calc.py and allows it to be called programatically.
         It contains sensible defaults for the arguments and abstracts some of the complexity away from 
         the underlying script. Configuring the PLL is not an exact science and there are many tradeoffs involved.
-        See generating_lut_guide.rst for some of the tradeoffs involved and some example paramater sets.
+        See sw_pll.rst for some of the tradeoffs involved and some example paramater sets.
 
         Once run, this function saves two output files:
         - fractions.h which contains the fractional term lookup table, which is guarranteed monotonic (important for PI stability)
@@ -566,11 +566,11 @@ ref_to_loop_call_rate   - Determines how often to call the control loop in terms
 xtal_frequency          - The xcore clock frequency
 nominal_ref_frequency   - The nominal input reference frequency
 target_output_frequency - The nominal target output frequency
-max_denom               - (Optional) The maximum fractional denominator. See/doc/generating_lut_guide.rst for guidance  
-min_F                   - (Optional) The minimum integer numerator. See/doc/generating_lut_guide.rst for guidance
-ppm_max                 - (Optional) The allowable PPM deviation for the target nominal frequency. See/doc/generating_lut_guide.rst for guidance
-fracmin                 - (Optional) The minimum  fractional multiplier. See/doc/generating_lut_guide.rst for guidance
-fracmax                 - (Optional) The maximum fractional multiplier. See/doc/generating_lut_guide.rst for guidance
+max_denom               - (Optional) The maximum fractional denominator. See/doc/sw_pll.rst for guidance  
+min_F                   - (Optional) The minimum integer numerator. See/doc/sw_pll.rst for guidance
+ppm_max                 - (Optional) The allowable PPM deviation for the target nominal frequency. See/doc/sw_pll.rst for guidance
+fracmin                 - (Optional) The minimum  fractional multiplier. See/doc/sw_pll.rst for guidance
+fracmax                 - (Optional) The maximum fractional multiplier. See/doc/sw_pll.rst for guidance
 """
 
 ref_to_loop_call_rate = 512
@@ -579,15 +579,17 @@ profile_choice = 0
 
 # Example profiles to produce typical frequencies seen in audio systems
 profiles = [
-    # 0 - 12.288MHz with 48kHz ref (note also works with 16kHz ref), +-150PPM, 29.3Hz steps, 426B LUT size
+    # 0 - 12.288MHz with 48kHz ref (note also works with 16kHz ref), +-250PPM, 29.3Hz steps, 426B LUT size
     {"nominal_ref_frequency":48000.0, "target_output_frequency":12288000, "max_denom":80, "min_F":200, "ppm_max":5, "fracmin":0.843, "fracmax":0.95},
     # 1 - 12.288MHz with 48kHz ref (note also works with 16kHz ref), +-500PPM, 30.4Hz steps, 826B LUT size
     {"nominal_ref_frequency":48000.0, "target_output_frequency":12288000, "max_denom":80, "min_F":200, "ppm_max":5, "fracmin":0.695, "fracmax":0.905},
-    # 2 - 24.576MHz with 48kHz ref (note also works with 16kHz ref), +-500PPM, 60.8Hz steps, 826B LUT size
-    {"nominal_ref_frequency":48000.0, "target_output_frequency":24576000, "max_denom":80, "min_F":200, "ppm_max":5, "fracmin":0.695, "fracmax":0.905},
-    # 3 - 24.576MHz with 48kHz ref (note also works with 16kHz ref), +-100PPM, 9.5Hz steps, 1050B LUT size
+    # 2 - 12.288MHz with 48kHz ref (note also works with 16kHz ref), +-500PPM, 30.4Hz steps, 826B LUT size
+    {"nominal_ref_frequency":48000.0, "target_output_frequency":12288000, "max_denom":80, "min_F":200, "ppm_max":5, "fracmin":0.695, "fracmax":0.905},
+    # 3 - 24.576MHz with 48kHz ref (note also works with 16kHz ref), +-1000PPM, 31.9Hz steps, 790B LUT size
+    {"nominal_ref_frequency":48000.0, "target_output_frequency":12288000, "max_denom":90, "min_F":140, "ppm_max":5, "fracmin":0.49, "fracmax":0.81},
+    # 4 - 24.576MHz with 48kHz ref (note also works with 16kHz ref), +-100PPM, 9.5Hz steps, 1050B LUT size
     {"nominal_ref_frequency":48000.0, "target_output_frequency":24576000, "max_denom":120, "min_F":400, "ppm_max":5, "fracmin":0.764, "fracmax":0.884},
-    # 4 - 6.144MHz with 16kHz ref, +-200PPM, 30.2Hz steps, 166B LUT size
+    # 5 - 6.144MHz with 16kHz ref, +-200PPM, 30.2Hz steps, 166B LUT size
     {"nominal_ref_frequency":16000.0, "target_output_frequency":6144000, "max_denom":40, "min_F":400, "ppm_max":5, "fracmin":0.635, "fracmax":0.806},
     ]
 
