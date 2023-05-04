ce3c766e94a18679f97ddf6e5c37b89834c91dad
Michael Banther <michaelb@LT000633.xmos.local>
2023-05-04 11:39:42 +0100

    Move to using fwk_io in the scripts provided for CI

--- tools/ci/checkout-submodules.sh
+++ tools/ci/checkout-submodules.sh
@@ -23,7 +23,7 @@ mkdir -p modules
 pushd modules
 clone infr_scripts_py git@github.com:xmos/infr_scripts_py.git 1d767cbe89a3223da7a4e27c283fb96ee2a279c9
 clone infr_apps       git@github.com:xmos/infr_apps.git       8bc62324b19a1ab32b1e5a5e262f40f710f9f5c1 
-clone xcore_sdk       git@github.com:xmos/xcore_sdk.git       a305fcf5ec3d00acd385ddee8fd681b0d855a059
+clone fwk_io          git@github.com:xmos/fwk_io.git          6b3275cbe4e39abce3b6e822655732767a62740d
 
 pip install -e infr_apps -e infr_scripts_py
 popd
