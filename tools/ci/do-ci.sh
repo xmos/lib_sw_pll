414a2ab6e687781605bba9e67e227cf427055d29
Michael Banther <michaelb@LT000633.xmos.local>
2023-05-05 11:43:30 +0100

    Try moving fwk_core and fwk_io into the modules directory

--- tools/ci/do-ci.sh
+++ tools/ci/do-ci.sh
@@ -4,7 +4,7 @@
 
 set -ex
 
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=fwk_io/xmos_cmake_toolchain/xs3a.cmake
+cmake -B build -DCMAKE_TOOLCHAIN_FILE=modules/fwk_io/xmos_cmake_toolchain/xs3a.cmake
 cmake --build build --target all --target test_app --target test_app_low_level_api --target simple --target i2s_slave  -j$(nproc)
 
 pushd tests
