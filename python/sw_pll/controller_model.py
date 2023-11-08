7b4eea248a4a1facd011ea0036e7bebed3f027b6
Ed <ed@xmos.com>
2023-11-08 12:17:33 +0000

    WIP decompose model into blocks

---
+++ python/sw_pll/controller_model.py
@@ -0,0 +1,87 @@
+# Copyright 2022-2023 XMOS LIMITED.
+# This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+import dco_model
+import numpy as np
+
+class sw_pll_lut_pi_ctrl(dco_model.lut_dco):
+    """
+        This class instantiates a control loop instance. It takes a lookup table function which can be generated 
+        from the error_from_h class which allows it use the actual pre-calculated transfer function.
+        Once instantiated, the do_control method runs the control loop.
+
+        This class forms the core of the simulator and allows the constants (K..) to be tuned to acheive the 
+        desired response. The function run_sim allows for a plot of a step resopnse input which allows this
+        to be done visually.
+    """
+    lock_status_lookup = {-1 : "UNLOCKED LOW", 0 : "LOCKED", 1 : "UNLOCKED HIGH"}
+
+    def __init__(self,  Kp, Ki, Kii=None, base_lut_index=None, verbose=False):
+
+        self.dco = dco_model.lut_dco()
+        self.lut_lookup_function = self.dco.get_lut()
+        lut_size = self.dco.get_lut_size()
+
+        # By default set the nominal LUT index to half way
+        if base_lut_index is None:
+            base_lut_index = lut_size // 2
+        self.base_lut_index = base_lut_index
+
+        self.Kp     = Kp
+        self.Ki     = Ki
+        self.Kii    = 0.0 if Kii is None else Kii
+
+        self.diff = 0.0                 # Most recent diff between expected and actual
+        self.error_accum = 0.0          # Integral of error
+        self.error_accum_accum = 0.0    # Double integral of error
+        self.total_error = 0.0          # Calculated total error
+
+        # Set windup limit to the lut_size, which by default is double of the deflection from nominal
+        self.i_windup_limit     = lut_size / self.Ki if self.Ki != 0.0 else 0.0
+        self.ii_windup_limit    = lut_size / self.Kii if self.Kii != 0.0 else 0.0
+
+        self.verbose = verbose
+
+        if verbose:
+            print(f"Init sw_pll_lut_pi_ctrl, Kp: {Kp} Ki: {Ki} Kii: {Kii}")
+
+    def get_dco_ctrl(self):
+        return self.error
+
+
+    def do_control_from_error(self, error):
+        """
+        Calculate the LUT setting from the input error
+        """
+        self.diff = error # Used by tests
+
+        # clamp integral terms to stop them irrecoverably drifting off.
+        self.error_accum = np.clip(self.error_accum + error, -self.i_windup_limit, self.i_windup_limit) 
+        self.error_accum_accum = np.clip(self.error_accum_accum + self.error_accum, -self.ii_windup_limit, self.ii_windup_limit) 
+
+        error_p  = self.Kp * error;
+        error_i  = self.Ki * self.error_accum
+        error_ii = self.Kii * self.error_accum_accum
+
+        self.error = error_p + error_i + error_ii
+
+        if self.verbose:
+            print(f"error: {error} error_p: {error_p} error_i: {error_i} error_ii: {error_ii} total error: {self.error}")
+
+        dco_ctrl = self.base_lut_index - self.error
+
+        return dco_ctrl
+
+
+
+if __name__ == '__main__':
+    """
+    This module is not intended to be run directly. This is here for internal testing only.
+    """
+    Kp = 1.0
+    Ki = 0.1
+    
+    sw_pll = sw_pll_lut_pi_ctrl(Kp, Ki, verbose=True)
+    for error_input in range(-10, 20):
+        dco_ctrl = sw_pll.do_control_from_error(error_input)
+
---
+++ python/sw_pll/dco_model.py
@@ -0,0 +1,259 @@
+# Copyright 2023 XMOS LIMITED.
+# This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+import app_pll_model
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import re
+
+"""
+This file contains implementations of digitally controlled oscillators.
+It uses the app_pll_model underneath to turn a control signal into a 
+calculated output frequency.
+
+It currently contains two implementations of DCO:
+
+-   A lookup table version which is efficient in computation and offers
+    a range of frequencies based on a pre-calculated look up table (LUT)
+-   A Sigma Delta Modulator which typically uses a dedicated thread to
+    run the modulator but results in lower noise in the audio spectrum
+"""
+
+
+##############################
+# LOOK UP TABLE IMPLEMENTATION
+##############################
+
+class lut_dco:
+    """ 
+        This class parses a pre-generated fractions.h file and builds a lookup table so that the values can be
+        used by the sw_pll simulation. It may be used directly but is generally used a sub class of error_to_pll_output_frequency.
+    """
+    def __init__(self, header_file = "fractions.h", force_lut_gen=False, verbose=False):   # fixed header_file name by pll_calc.py 
+        """
+        Constructor for the LUT DCO. Reads the pre-calculated header file and produces the LUT which contains
+        the pll fractional register settings (16b) for each of the entries
+        """
+        if not os.path.exists(header_file):
+            assert False, f"Please initialize a lut_dco to produce a parsable header file {header_file}"
+        with open(header_file) as hdr:
+            header = hdr.readlines()
+            min_frac = 1.0
+            max_frac = 0.0
+            for line in header:
+                regex_ne = fr"frac_values_?\d*\[(\d+)].*"
+                match = re.search(regex_ne, line)
+                if match:
+                    num_entries = int(match.groups()[0])
+                    # print(f"num_entries: {num_entries}")
+                    lut = np.zeros(num_entries, dtype=np.uint16)
+
+                regex_fr = r"0x([0-9A-F]+).+Index:\s+(\d+).+=\s(0.\d+)"
+                match = re.search(regex_fr, line)
+                if match:
+                    reg, idx, frac = match.groups()
+                    reg = int(reg, 16)
+                    idx = int(idx)
+                    frac = float(frac)
+                    min_frac = frac if frac < min_frac else min_frac
+                    max_frac = frac if frac > max_frac else max_frac
+                    lut[idx] = reg
+
+            # print(f"min_frac: {min_frac} max_frac: {max_frac}")
+
+            self.lut = lut
+            self.min_frac = min_frac
+            self.max_frac = max_frac
+
+
+        # if not os.path.exists(app_pll_model.register_file) or not os.path.exists(header_file) or force_lut_gen:
+        #     pll = app_pll_model.app_pll_frac_calc(app_pll_model.register_file)
+
+        input_freq, F, R, f, p, OD, ACD = self.parse_register_file(app_pll_model.register_file)
+        self.app_pll = app_pll_model.app_pll_frac_calc(input_freq, F, R, f, p, OD, ACD)
+
+    def parse_register_file(self, register_file):
+        """
+            This method reads the pre-saved register setup comments from get_pll_solution and parses them into parameters that
+            can be used for later simulation.
+        """
+
+        with open(register_file) as rf:
+            reg_file = rf.read().replace('\n', '')
+            input_freq = int(re.search(".+Input freq:\s+(\d+).+", reg_file).groups()[0])
+            F = int(re.search(".+F:\s+(\d+).+", reg_file).groups()[0])
+            R = int(re.search(".+R:\s+(\d+).+", reg_file).groups()[0])
+            f = int(re.search(".+f:\s+(\d+).+", reg_file).groups()[0])
+            p = int(re.search(".+p:\s+(\d+).+", reg_file).groups()[0])
+            OD = int(re.search(".+OD:\s+(\d+).+", reg_file).groups()[0])
+            ACD = int(re.search(".+ACD:\s+(\d+).+", reg_file).groups()[0])
+
+        return input_freq, F, R, f, p, OD, ACD
+
+    def get_lut(self):
+        """
+        Return the look up table
+        """
+        return self.lut
+
+    def get_lut_size(self):
+        """
+        Return the size of look up table
+        """
+        return np.size(self.lut)
+
+    def print_stats(self):
+        """
+        Returns a summary of the LUT range and steps.
+        """
+        lut = self.get_lut()
+        steps = np.size(lut)
+
+        register = int(lut[0])
+        min_freq = self.app_pll.update_frac_reg(register)
+
+        register = int(lut[steps // 2])
+        mid_freq = self.app_pll.update_frac_reg(register)
+
+        register = int(lut[-1])
+        max_freq = self.app_pll.update_frac_reg(register)
+
+        print(f"LUT min_freq: {min_freq}")
+        print(f"LUT mid_freq: {mid_freq}")
+        print(f"LUT max_freq: {max_freq}")
+        print(f"LUT steps: {steps}")
+
+        return min_freq, mid_freq, max_freq, steps
+
+    def _reg_to_frac(self, register):
+        f = (register & 0xff00) >> 8
+        p = register & 0xff
+
+        return f, p
+
+
+    def plot_freq_range(self):
+        """
+        Generates a plot of the frequency range of the LUT and
+        visually shows the spacing of the discrete frequencies
+        that it can produce.
+        """
+
+        frequencies = []
+        for step in range(self.get_lut_size()):
+            register = int(self.lut[step])
+            self.app_pll.update_frac_reg(register)
+            frequencies.append(self.app_pll.get_output_frequency())
+
+        plt.clf()
+        plt.plot(frequencies, color='green', marker='.', label='frequency')
+        plt.title('PLL fractional range', fontsize=14)
+        plt.xlabel(f'LUT index', fontsize=14)
+        plt.ylabel('Frequency', fontsize=10)
+        plt.legend(loc="upper right")
+        plt.grid(True)
+        # plt.show()
+        plt.savefig("lut_dco_range.png", dpi=150)
+
+    # def get_frequency_from_error(error, lut, app_pll_frac_calc):
+    #     """
+    #     given an error, a lut, and a pll, calculate the frequency
+    #     """
+    #     num_entries = np.size(lut)
+
+    #     set_point = int(error) #  Note negative term for neg feedback
+    #     if set_point < 0:
+    #         set_point = 0
+    #         lock_status = -1
+    #     elif set_point >= num_entries:
+    #         set_point = num_entries - 1
+    #         lock_status = 1
+    #     else:
+    #         set_point = set_point
+    #         lock_status = 0
+
+    #     register = int(lut[set_point])
+    #     pll.update_pll_frac_reg(register)
+
+    #     return pll.get_output_frequency(), lock_status
+
+# class error_to_pll_output_frequency(app_pll_frac_calc, parse_lut_h_file):
+#     """ 
+#         This super class combines app_pll_frac_calc and parse_lut_h_file and provides a way of inputting the error signal and
+#         providing an output frequency for a given set of PLL configuration parameters. It includes additional methods for
+#         turning the LUT register settings parsed by parse_lut_h_file into fractional values which can be fed into app_pll_frac_calc.
+
+#         It also contains information reporting methods which provide the range and step sizes of the PLL configuration as well as 
+#         plotting the transfer function from error to frequency so the linearity and regularity the transfer function can be observed.
+#     """
+
+#     def __init__(self, header_file, input_frequency, F_init, R_init, OD_init, ACD_init, f_init, p_init, verbose=False):
+#         self.app_pll_frac_calc = app_pll_frac_calc.__init__(self, input_frequency, F_init, R_init, OD_init, ACD_init, f_init, p_init, verbose=False)
+#         self.parse_lut_h_file = parse_lut_h_file.__init__(self, header_file, verbose=False)
+#         self.verbose = verbose
+
+
+
+
+# def _get_output_frequency_from_error(self, error):
+#     lut = self.get_lut()
+
+#     return get_frequency_from_error(error, lut, self)
+
+######################################
+# SIGMA DELTA MODULATOR IMPLEMENTATION
+######################################
+
+class sdm:
+    """
+    Experimental - taken from lib_xua synchronous branch
+    Third order, 9 level output delta sigma. 20 bit unsigned input.
+    """
+    def __init__(self):
+        # Delta sigma modulator state
+        self.ds_x1 = 0
+        self.ds_x2 = 0
+        self.ds_x3 = 0
+
+
+    # generalized version without fixed point shifts. WIP!!
+    # takes a Q20 number from 
+    def do_sigma_delta(self, ds_in):
+        ds_out = int(self.ds_x3 * 0.002197265625)
+        if ds_out > 8:
+            ds_out = 8
+        if ds_out < 0:
+            ds_out = 0
+        self.ds_x3 += int((self.ds_x2 * 0.03125) - (ds_out * 768))
+        self.ds_x2 += int((self.ds_x1 * 0.03125) - (ds_out * 16384))
+        self.ds_x1 += int(ds_in - (ds_out * 131072))
+
+        return ds_out
+
+
+class sigma_delta_dco(sdm):
+    """
+    TBD
+    """
+    def __init__(self):
+        # input_freq =
+        # F =
+        # R = 
+        # f =
+        # p = 
+        # OD
+        # ACD = 
+        pass
+
+
+
+if __name__ == '__main__':
+    """
+    This module is not intended to be run directly. This is here for internal testing only.
+    """
+    dco = lut_dco()
+    print(f"LUT size: {dco.get_lut_size()}")
+    print(f"LUT : {dco.get_lut()}")
+    dco.plot_freq_range()
+    dco.print_stats()
