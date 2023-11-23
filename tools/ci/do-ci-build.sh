67729a5cd0c604ad217e501f88bd17c186ade6ec
Ed <ed@xmos.com>
2023-11-23 08:47:51 +0000

    Add SDM tests to build

--- tools/ci/do-ci-build.sh
+++ tools/ci/do-ci-build.sh
@@ -5,4 +5,4 @@
 set -ex
 
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=modules/fwk_io/xmos_cmake_toolchain/xs3a.cmake
-cmake --build build --target all --target test_app --target test_app_low_level_api --target simple --target i2s_slave  -j$(nproc)
+cmake --build build --target all --target test_app --target test_app_low_level_api --target test_app_sdm_dco --target test_app_sdm_ctrl --target simple --target i2s_slave  -j$(nproc)
