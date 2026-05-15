b02db780a2a902281cd3f5607b7e2a31bc59eb7e
Ross Owen <ross@xmos.com>
2026-05-15 12:13:48 +0100

    Fix implicit int16_t conversion warnings on negation

--- lib_sw_pll/src/sw_pll_pfd.h
+++ lib_sw_pll/src/sw_pll_pfd.h
@@ -8,13 +8,13 @@
 
 #pragma once
 
-#define SW_PLL_PFD_PRE_DIV_BITS 37 // Used pre-computing a divide to save on runtime div usage. Tradeoff between precision and max 
+#define SW_PLL_PFD_PRE_DIV_BITS 37 // Used pre-computing a divide to save on runtime div usage. Tradeoff between precision and max
 
 /**
  * Initialises the Phase Frequency Detector.
  *
  * Sets how often the PFD and control loop runs and error limits for resetting
- * 
+ *
  * \param pfd_state             Pointer to the struct to be initialised.
  * \param loop_rate_count       How many counts of the call to sw_pll_lut_do_control before control is done.
  *                              Note this is only used by sw_pll_lut_do_control. sw_pll_lut_do_control_from_error
@@ -30,7 +30,7 @@
  *                              of counted mclk before the PLL resets its state.
  *                              Note this is only used by sw_pll_lut_do_control. sw_pll_lut_do_control_from_error
  *                              calls the control loop every time so this is ignored.
- */ 
+ */
 void sw_pll_pfd_init(sw_pll_pfd_state_t *pfd_state,
                     const size_t loop_rate_count,
                     const size_t pll_ratio,
@@ -44,7 +44,7 @@ void sw_pll_pfd_init(sw_pll_pfd_state_t *pfd_state,
  * \param first_loop            Pointer to the first_loop var used for resetting when out of range.
  * \param mclk_pt               The clock count from the port counter.
  * \param ref_clk_pt            The clock count from the ref clock counter (optional).
- */ 
+ */
 __attribute__((always_inline))
 static inline void sw_pll_calc_error_from_port_timers(  sw_pll_pfd_state_t * const pfd,
                                                         uint8_t *first_loop,
@@ -57,13 +57,14 @@ static inline void sw_pll_calc_error_from_port_timers(  sw_pll_pfd_state_t * con
     {
         uint16_t ref_clk_expected_pt = (uint16_t)(pfd->ref_clk_pt_last + pfd->ref_clk_expected_inc);
         // This uses casting trickery to work out the difference between the timer values accounting for wrap at 65536
-        int16_t ref_clk_diff = PORT_TIMEAFTER(ref_clk_pt, ref_clk_expected_pt) ? -(int16_t)(ref_clk_expected_pt - ref_clk_pt) : (int16_t)(ref_clk_pt - ref_clk_expected_pt);
+        int16_t pt_diff = (int16_t)(ref_clk_expected_pt - ref_clk_pt);
+        int16_t ref_clk_diff = PORT_TIMEAFTER(ref_clk_pt, ref_clk_expected_pt) ? (int16_t)(-pt_diff) : (int16_t)(ref_clk_pt - ref_clk_expected_pt);
         pfd->ref_clk_pt_last = ref_clk_pt;
 
         // This allows for wrapping of the timer when CONTROL_LOOP_COUNT is high
         // Note we use a pre-computed divide followed by a shift to replace a constant divide with a constant multiply + shift
         uint32_t mclk_expected_pt_inc = ((uint64_t)pfd->mclk_expected_pt_inc
-                                         * ((uint64_t)pfd->ref_clk_expected_inc + (uint64_t)ref_clk_diff) 
+                                         * ((uint64_t)pfd->ref_clk_expected_inc + (uint64_t)ref_clk_diff)
                                          * pfd->ref_clk_scaling_numerator) >> SW_PLL_PFD_PRE_DIV_BITS;
         // Below is the line we would use if we do not pre-compute the divide. This can take a long time if we spill over 32b
         // uint32_t mclk_expected_pt_inc = pfd->mclk_expected_pt_inc * (pfd->ref_clk_expected_inc + ref_clk_diff) / pfd->ref_clk_expected_inc;
@@ -75,7 +76,7 @@ static inline void sw_pll_calc_error_from_port_timers(  sw_pll_pfd_state_t * con
     }
 
     // This uses casting trickery to work out the difference between the timer values accounting for wrap at 65536
-    pfd->mclk_diff = PORT_TIMEAFTER(mclk_pt, mclk_expected_pt) ? -(int16_t)(mclk_expected_pt - mclk_pt) : (int16_t)(mclk_pt - mclk_expected_pt);
+    pfd->mclk_diff = PORT_TIMEAFTER(mclk_pt, mclk_expected_pt) ? (int16_t)(-((int16_t)(mclk_expected_pt - mclk_pt))) : (int16_t)(mclk_pt - mclk_expected_pt);
 
     // Check to see if something has gone very wrong, for example ref clock stop/start. If so, reset state and keep trying
     if(MAGNITUDE(pfd->mclk_diff) > pfd->mclk_max_diff)
